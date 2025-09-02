49a9db41cf9cb6d469ef421bcd3ffbbb39543e03
thomas351 <thomas85@mail.ch>
2025-09-02 20:45:27 +0200

    add more documentation to diesel_generate.sh script

--- diesel_generate.sh
+++ diesel_generate.sh
@@ -12,9 +12,12 @@ diesel migration run
 
 # feature sets of LukasLohmar & abbychau merged:
 #cargo install --git "https://github.com/thomas725/diesel_cli_ext" --branch master
+# todo: switch to upstream once it supports "struct-attribute" parameters or an equivalent.
+# see also: https://github.com/abbychau/diesel_cli_ext/issues/67
 
 #cargo install --git "https://github.com/LukasLohmar/diesel_cli_ext" --branch dev
 diesel_ext --import-types "crate::schema::*" --add-table-name --struct-attribute "#[diesel(check_for_backend(diesel::sqlite::Sqlite))]" --derive "Queryable, Selectable, Debug" > src/models_queryable.rs
 
 #cargo install diesel_cli_ext
 diesel_ext --import-types "crate::schema::*" --import-types "diesel::Insertable" --insertable > src/models_insertable.rs
+# WARNING: needs manual modification: delete <'a> from structs without borrowed data.
